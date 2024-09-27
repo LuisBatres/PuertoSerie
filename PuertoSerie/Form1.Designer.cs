937c3d781e326b89cdcfeaeac116d9b5d773fc0a
LuisBatres <alu.20130806@correo.itlalaguna.edu.mx>
2024-09-27 15:21:32 -0600

    last update

--- PuertoSerie/Form1.Designer.cs
+++ PuertoSerie/Form1.Designer.cs
@@ -36,30 +36,33 @@
             txtEnviar = new TextBox();
             btnEnviar = new Button();
             btnActualizar = new Button();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(14, 12);
             label1.Name = "label1";
-            label1.Size = new Size(70, 15);
+            label1.Size = new Size(89, 20);
             label1.TabIndex = 0;
             label1.Text = "Puerto Serie";
             // 
             // cmbPuertos
             // 
             cmbPuertos.FormattingEnabled = true;
-            cmbPuertos.Location = new Point(12, 27);
+            cmbPuertos.Location = new Point(14, 36);
+            cmbPuertos.Margin = new Padding(3, 4, 3, 4);
             cmbPuertos.Name = "cmbPuertos";
-            cmbPuertos.Size = new Size(274, 23);
+            cmbPuertos.Size = new Size(313, 28);
             cmbPuertos.TabIndex = 1;
             // 
             // btnConectar
             // 
-            btnConectar.Location = new Point(292, 26);
+            btnConectar.Location = new Point(334, 35);
+            btnConectar.Margin = new Padding(3, 4, 3, 4);
             btnConectar.Name = "btnConectar";
-            btnConectar.Size = new Size(75, 23);
+            btnConectar.Size = new Size(86, 31);
             btnConectar.TabIndex = 2;
             btnConectar.Text = "Conectar";
             btnConectar.UseVisualStyleBackColor = true;
@@ -68,31 +71,34 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 74);
+            label2.Location = new Point(14, 99);
             label2.Name = "label2";
-            label2.Size = new Size(0, 15);
+            label2.Size = new Size(0, 20);
             label2.TabIndex = 3;
             // 
             // rtbLog
             // 
-            rtbLog.Location = new Point(12, 56);
+            rtbLog.Location = new Point(14, 75);
+            rtbLog.Margin = new Padding(3, 4, 3, 4);
             rtbLog.Name = "rtbLog";
-            rtbLog.Size = new Size(436, 133);
+            rtbLog.Size = new Size(498, 176);
             rtbLog.TabIndex = 4;
             rtbLog.Text = "";
             // 
             // txtEnviar
             // 
-            txtEnviar.Location = new Point(12, 206);
+            txtEnviar.Location = new Point(14, 275);
+            txtEnviar.Margin = new Padding(3, 4, 3, 4);
             txtEnviar.Name = "txtEnviar";
-            txtEnviar.Size = new Size(346, 23);
+            txtEnviar.Size = new Size(395, 27);
             txtEnviar.TabIndex = 5;
             // 
             // btnEnviar
             // 
-            btnEnviar.Location = new Point(373, 205);
+            btnEnviar.Location = new Point(426, 273);
+            btnEnviar.Margin = new Padding(3, 4, 3, 4);
             btnEnviar.Name = "btnEnviar";
-            btnEnviar.Size = new Size(75, 23);
+            btnEnviar.Size = new Size(86, 31);
             btnEnviar.TabIndex = 6;
             btnEnviar.Text = "Enviar";
             btnEnviar.UseVisualStyleBackColor = true;
@@ -100,19 +106,31 @@
             // 
             // btnActualizar
             // 
-            btnActualizar.Location = new Point(373, 26);
+            btnActualizar.Location = new Point(426, 35);
+            btnActualizar.Margin = new Padding(3, 4, 3, 4);
             btnActualizar.Name = "btnActualizar";
-            btnActualizar.Size = new Size(75, 23);
+            btnActualizar.Size = new Size(86, 31);
             btnActualizar.TabIndex = 7;
             btnActualizar.Text = "Actualizar";
             btnActualizar.UseVisualStyleBackColor = true;
             btnActualizar.Click += btnActualizar_Click;
             // 
+            // button1
+            // 
+            button1.Location = new Point(518, 37);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 8;
+            button1.Text = "Desconectar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // frmSerie
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(460, 259);
+            ClientSize = new Size(676, 345);
+            Controls.Add(button1);
             Controls.Add(btnActualizar);
             Controls.Add(btnEnviar);
             Controls.Add(txtEnviar);
@@ -121,6 +139,7 @@
             Controls.Add(btnConectar);
             Controls.Add(cmbPuertos);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "frmSerie";
             Text = "Puerto Serie";
             Load += frmSerie_Load;
@@ -138,5 +157,6 @@
         private TextBox txtEnviar;
         private Button btnEnviar;
         private Button btnActualizar;
+        private Button button1;
     }
 }
--- PuertoSerie/Form1.cs
+++ PuertoSerie/Form1.cs
@@ -3,8 +3,6 @@ namespace PuertoSerie
 {
     public partial class frmSerie : Form
     {
-
-
         SerialPort sp = new SerialPort();
 
         public frmSerie()
@@ -14,48 +12,124 @@ namespace PuertoSerie
 
         private void frmSerie_Load(object sender, EventArgs e)
         {
-            string[] puertos = SerialPort.GetPortNames(); cmbPuertos.DataSource = SerialPort.GetPortNames();
+            cmbPuertos.DataSource = SerialPort.GetPortNames();
         }
 
         private void btnEnviar_Click(object sender, EventArgs e)
         {
-            sp.WriteLine(txtEnviar.Text);
-            txtEnviar.Clear();
+            if (sp.IsOpen)
+            {
+                try
+                {
+                    /*
+                    // Enviar texto desde el cuadro de texto a través del puerto serie
+                    sp.WriteLine(txtEnviar.Text);
+                    rtbLog.AppendText("Enviado: " + txtEnviar.Text + Environment.NewLine);
+                    txtEnviar.Clear();
+                    */
+                    // Verifica si el texto enviado es 'a' o 's'
+                    string mensaje = txtEnviar.Text;  // Convierte el texto a minúsculas para evitar errores de mayúsculas
+
+                    if (mensaje == "S" || mensaje == "N")
+                    {
+                        sp.WriteLine(mensaje);  // Envía el texto al puerto serie
+                        rtbLog.AppendText("Enviado: " + mensaje + Environment.NewLine);
+                        txtEnviar.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Por favor ingresa 'a' para encender o 's' para apagar el LED.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al enviar datos: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Primero debes conectar el puerto serie.");
+            }
         }
 
         private void btnConectar_Click(object sender, EventArgs e)
         {
-            try
+            if (!sp.IsOpen)
             {
+<<<<<<< Updated upstream
                 sp.PortName = cmbPuertos.Text;
                 sp.Open();
                 MessageBox.Show("Conectado");
                 sp.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+=======
+                try
+                {
+                    // Configurar el puerto serial
+                    sp.PortName = cmbPuertos.Text;
+                    sp.BaudRate = 9600;  // Configura la velocidad en baudios
+                    /*sp.DataBits = 8;
+                    sp.Parity = Parity.None;
+                    sp.StopBits = StopBits.One;*/
+>>>>>>> Stashed changes
 
+                    // Abrir el puerto y asignar el evento de recepción de datos
+                    sp.Open();
+                    sp.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
 
+                    MessageBox.Show("Puerto conectado exitosamente.");
+                    btnConectar.Enabled = false;
+                    button1.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar el puerto: " + ex.Message);
+                }
             }
-            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
         }
 
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
+            // Este método se ejecuta cuando se reciben datos a través del puerto serial
+            try
+            {
+                SerialPort actualSP = (SerialPort)sender;
 
-            SerialPort actualSP = (SerialPort)sender;
-
-
-            string inData = actualSP.ReadLine();
-            rtbLog.Invoke(new MethodInvoker(
-                        delegate
-                        {
-                            rtbLog.Text = rtbLog.Text + inData;
-                        }
-
-                        ));
+                // Leer los datos recibidos del puerto serial
+                string inData = actualSP.ReadLine();
+                rtbLog.Invoke(new MethodInvoker(
+                    delegate
+                    {
+                        // Mostrar los datos recibidos en el log
+                        rtbLog.AppendText(inData + "\n");
+                    }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al recibir datos: " + ex.Message);
+            }
         }
 
         private void btnActualizar_Click(object sender, EventArgs e)
         {
-            string[] puertos = SerialPort.GetPortNames(); cmbPuertos.DataSource = SerialPort.GetPortNames();
+            cmbPuertos.DataSource = SerialPort.GetPortNames();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                try
+                {
+                    sp.Close();
+                    MessageBox.Show("Puerto desconectado exitosamente.");
+                    btnConectar.Enabled = true;
+                    button1.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al desconectar el puerto: " + ex.Message);
+                }
+            }
         }
     }
 }
